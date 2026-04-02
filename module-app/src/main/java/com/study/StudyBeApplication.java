7163b3c3f1b7e912c6eceb6f6eb61f94338b1184
xhae123 <xhae000@gmail.com>
2026-04-02 16:42:05 +0900

    style: Spotless 전체 코드베이스 포맷 보정

--- module-app/src/main/java/com/study/StudyBeApplication.java
+++ module-app/src/main/java/com/study/StudyBeApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StudyBeApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(StudyBeApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(StudyBeApplication.class, args);
+  }
 }
--- module-qna/src/main/java/com/study/qna/package-info.java
+++ module-qna/src/main/java/com/study/qna/package-info.java
@@ -1,4 +1,2 @@
-/**
- * QnA 모듈 - 질문/답변 관련 기능
- */
+/** QnA 모듈 - 질문/답변 관련 기능 */
 package com.study.qna;
