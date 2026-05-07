e7152aabea9d04e6941f38e287e0e67590176f12
심아현 <simahyeon42@gmail.com>
2026-05-07 16:59:30 +0900

    fix: qna comment bean 이름 충돌 수정

--- src/main/java/com/study/qna/infrastructure/CommentRepository.java
+++ src/main/java/com/study/qna/infrastructure/CommentRepository.java
@@ -1,8 +1,11 @@
 package com.study.qna.infrastructure;
 import com.study.qna.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
+@Repository("qnaCommentRepository")
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findByAnswer_IdOrderByCreatedAtAsc(Long answerId);
 }
