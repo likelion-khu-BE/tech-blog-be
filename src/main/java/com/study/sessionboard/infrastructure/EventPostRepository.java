bb90b0e672ce4aee9cb229d45468d26f83868576
한예진 <han-yejin@han-yejin-ui-MacBookAir.local>
2026-05-08 17:08:04 +0900

    chore: dev 브랜치 리베이스 완료 및 충돌 해결

--- src/main/java/com/study/sessionboard/infrastructure/EventPostRepository.java
+++
@@ -1,8 +0,0 @@
-package com.study.sessionboard.infrastructure;
-
-import com.study.sessionboard.domain.event.EventPost;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EventPostRepository extends JpaRepository<EventPost, Long> {}
