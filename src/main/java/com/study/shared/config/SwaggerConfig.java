e65d6b9107c210b642ed8ae39b0e55f174ea2f14
Suuunz <ysj18805@khu.ac.kr>
2026-05-12 14:00:57 +0900

    feat: Swagger UI 설정 추가 및 Security 경로 허용

---
+++ src/main/java/com/study/shared/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.study.shared.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+  @Bean
+  public OpenAPI openAPI() {
+    SecurityScheme bearerAuth =
+        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT");
+
+    return new OpenAPI()
+        .info(
+            new Info()
+                .title("Tech Blog API")
+                .description("경희대 멋쟁이사자처럼 Tech Blog API")
+                .version("v1"))
+        .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+        .components(new Components().addSecuritySchemes("bearerAuth", bearerAuth));
+  }
+}
